829b95477c440b3d3c9cdbea210ca9d6e625e881
Jun <junanthony.business@gmail.com>
2025-03-05 09:41:30 +1300

    Add project files.

---
+++ CozyToDoAppApi/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using CozyToDoAppApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CozyToDoAppApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<TaskItem> TaskItems { get; set; }      
+    }
+}
---
+++ CozyToDoAppApi/Models/TaskItem.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CozyToDoAppApi.Models
+{
+    public class TaskItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; }
+    }
+}
